ebb90e8e2a2b664f261eaee5b29949fbc3a3764d
Wilfred <wilfred.s.lim@gmail.com>
2019-09-12 09:55:22 -0700

    we

--- ScanPractice.java
+++ ScanPractice.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.text.DecimalFormat;
 
 /**
  * Write a description of class ScanPractice here.
@@ -12,14 +11,14 @@ public class ScanPractice
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter a line of text:");
-        
+
         String word = scan.next();
-        
+
         String line = scan.nextLine();
-        
+
         System.out.println(word);
         System.out.println(line);
-        
+
         // DecimalFormat fmt = new DecimalFormat("0.###");
         // System.out.println("A formatted decimal: " + fmt.format(15.2346672));
         // fmt.applyPattern("0.#");
