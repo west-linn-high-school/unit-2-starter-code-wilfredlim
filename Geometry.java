e1e58446140dad705328ebd00a4c3c08ed7fd6d1
Wilfred <wilfred.s.lim@gmail.com>
2019-09-12 09:44:48 -0700

    good good good

--- Geometry.java
+++ Geometry.java
@@ -3,20 +3,20 @@ public class Geometry{
     int sides=7;
     int radius=5;
     final double PI=22/7;
-    
+
     int c;
-    int a = 40;
-    int b = 6;
+    int a = 60;
+    int b = 5;
     double y = 6.0;
     double z;
-    
+
     z = (double) (a/b);
-    
+
     System.out.println("z= " + z + "");
     System.out.println("A heptaon has " + sides + " sides.");
     System.out.println("A decagon has " + (sides + 3) + " sides.");
     System.out.println("A dodecagon has " + (sides + radius) + " sides.");
     System.out.print("The circumference of a circel with a radius");
     System.out.print(" of " + radius + " is " + 2*PI*radius + ".");
-} 
+}
 }
