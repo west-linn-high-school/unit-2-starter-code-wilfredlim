c8630759e518c70a55309985aa35886cf5b24a75
Wilfred <wilfred.s.lim@gmail.com>
2019-09-17 08:45:55 -0700

    new nwe

--- ScanNumbers.java
+++ ScanNumbers.java
@@ -3,7 +3,7 @@ public class ScanNumbers
 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a two numbers!");
+        System.out.println("Enter a two numbers! (with a space or enter between numbers)" );
 
         double num1 = scan.nextInt();
         double num2 = scan.nextInt();
@@ -14,7 +14,7 @@ public class ScanNumbers
         double quot = (num1/num2);
 
         System.out.println("The sum of " + num1 + " and " + num2 + " is = " + sum);
-        System.out.println("The differnce of " + num1 + " and " + num2 + " is = " + diff);
+        System.out.println("The difference of " + num1 + " and " + num2 + " is = " + diff);
         System.out.println("The product of " + num1 + " and " + num2 + " is = " + prod);
         System.out.println("The quotient of " + num1 + " and " + num2 + " is = " + quot);
     }
